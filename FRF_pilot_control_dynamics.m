d5701565bc68b6c04ca4f7acb8df31e1a17ebfdc
Mayukh Sarkar <mayukhsarkar66@gmail.com>
2021-05-22 11:54:35 +0200

    update part 1

--- FRF_pilot_control_dynamics.m
+++ FRF_pilot_control_dynamics.m
@@ -3,9 +3,9 @@ load('ae4320_dataset1.mat')
 dt = t(2) -t(1);
 N1 = length(t);
 Fs = 1/dt;
-freq = (Fs/N1)*(1:N1);
+freq = (Fs/N1)*(1:N1);% freq in HZ
 omega = 2*pi*freq;
-[ft_pks, ft_pklocs] = findpeaks(ft);
+[ft_pks, ft_pklocs] = findpeaks(ft); % identifying the excitation freq. Peaks of the forcing function
 figure(1)
 subplot(2,1,1)
 plot(freq,ft,freq(ft_pklocs),ft_pks,'or')
@@ -29,24 +29,33 @@ xlabel('time')
 ylabel('forcing signal')
 %axis tight
 
-%% Estimate of pilot control dynamics
+%% Estimate of cross psd
 error = identification.e;
 input = identification.u;
 N = length(ft_pks);
-e_dft = fft(error(ft_pklocs),N);
-u_dft = fft(input(ft_pklocs),N);
-ft_dft = fft(ft_pks,N);
-Seft = conj(e_dft(1:N)).*ft_dft(1:N)/N;
-Suft = conj(u_dft(1:N)).*ft_dft(1:N)/N;
+Freq = (Fs/N)*(1:N);
 
+e_dft = fft(error(ft_pklocs),N); % Fourier transform of the error signal at excitation freq
+u_dft = fft(input(ft_pklocs),N); % Fourier transform of the input signal at excitation freq
+ft_dft = fft(ft_pks,N);  % Fourier transform of the forcing function
+Seft = conj(e_dft(1:N)).*ft_dft(1:N)/N; % cross power spectral density of error and ft
+Suft = conj(u_dft(1:N)).*ft_dft(1:N)/N; %cross power spectral density of input and ft
 
-Hp = Suft./Seft;
-magh = abs(Hp);
-phaseh = 180*angle(Hp)/pi;
 figure(3)
-loglog( freq(ft_pklocs), magh) ;
+subplot(2,1,1)
+loglog(Freq,abs(Seft))
+subplot(2,1,2)
+loglog(Freq,abs(Suft))
+
+
+%% Pilot dynamics
+Hp = Suft./Seft; %pilot dynamics at excitation freq
+magh = abs(Hp); % magnitude of Hp
+phaseh = 180*angle(Hp)/pi; % phase of hp
 figure(4)
-semilogx( freq(ft_pklocs), phaseh) ;
+loglog( Freq, magh) ;
+figure(5)
+semilogx( Freq, phaseh) ;
 
 
 
